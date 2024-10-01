4284aeacd9dd9d9a0cb4b739d2ce5adcf6442a03
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-10-01 11:25:32 +0200

    fix scrolling changing the page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,11 +4,13 @@ import { Home } from "./pages/Home/Home";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Form } from "./pages/Form/Form";
 import { Header } from "./components/Header/Header";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
+      <ScrollToTop />
         <div className="App">
           <Header />
           <Routes>
--- FE/src/pages/Home/Home.tsx
+++ FE/src/pages/Home/Home.tsx
@@ -11,11 +11,6 @@ export const Home = () => {
 
   const navigate = useNavigate();
 
-  const navigateToForm = () => {
-    navigate("/form");
-    window.scroll(0,0);
-  }
-
   return (
     <div>
       <main>
@@ -75,7 +70,10 @@ export const Home = () => {
           alignment="right"
         />
         <Text text={t("text.your-election-msg")} />
-        <Button label={t("button.fill-form")} onClick={navigateToForm}></Button>
+        <Button
+          label={t("button.fill-form")}
+          onClick={() => navigate("/form")}
+        ></Button>
         <h1>
           <img src="/img/party.png" alt="party icon" /> {t("title.waiting")}{" "}
           <img src="/img/party.png" alt="party icon" />
