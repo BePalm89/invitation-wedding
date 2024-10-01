081f6e2ecf9599453f94bbaaf6835ca5d3509cd1
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-10-01 11:44:16 +0200

    remove clog + add an arrow to go back to home page

--- FE/src/components/Header/Header.tsx
+++ FE/src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { LANGUAGES } from "../../constants";
 import "./Header.css";
 import {
@@ -11,9 +12,14 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 export const Header = () => {
   const { i18n } = useTranslation();
+
+  const { pathname } = useLocation();
+
+  const navigate = useNavigate();
 
   const onChangeLang = (lang_code: string) => {
     i18n.changeLanguage(lang_code);
@@ -21,29 +27,40 @@ export const Header = () => {
 
   const currentLang = i18n.language || "es";
 
-  const currentLanguage = LANGUAGES.find(lang => lang.code === currentLang);
+  const currentLanguage = LANGUAGES.find((lang) => lang.code === currentLang);
+
+  const isHome = pathname === "/";
 
   return (
     <header>
-      <Menu>
-        <MenuButton as={Button} size='sm' width="max-content">
-          <HStack>
-            <Image boxSize="20px" src={currentLanguage?.icon} alt={`${currentLang} flag`} />
-            <Text>{currentLanguage?.label || "Language"}</Text>
-          </HStack>
-        </MenuButton>
-        
-        <MenuList className="menu-list">
-          {LANGUAGES.map(({ code, label, icon }) => (
-            <MenuItem key={code} onClick={() => onChangeLang(code)}>
-              <HStack>
-                <Image boxSize="20px" src={icon} alt={`${label} flag`} />
-                <Text>{label}</Text>
-              </HStack>
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+      <div className="header-left">
+        {!isHome && <ArrowBackIcon boxSize={6} onClick={() => navigate("/")} />}
+      </div>
+      <div className="header-right">
+        <Menu>
+          <MenuButton as={Button} size="sm" width="max-content">
+            <HStack>
+              <Image
+                boxSize="20px"
+                src={currentLanguage?.icon}
+                alt={`${currentLang} flag`}
+              />
+              <Text>{currentLanguage?.label || "Language"}</Text>
+            </HStack>
+          </MenuButton>
+
+          <MenuList className="menu-list">
+            {LANGUAGES.map(({ code, label, icon }) => (
+              <MenuItem key={code} onClick={() => onChangeLang(code)}>
+                <HStack>
+                  <Image boxSize="20px" src={icon} alt={`${label} flag`} />
+                  <Text>{label}</Text>
+                </HStack>
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </div>
     </header>
   );
 };
